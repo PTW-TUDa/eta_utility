edd032983bad2aacc3237ed1439725c97c7de52f
Nayyer, Saahil <saahil.nayyer@stud.tu-darmstadt.de>
2021-07-10 00:34:25 +0000

    Resolve "Make sure that correct modbus values are read (little endian vs. big endian)"

--- test/test_utilities/pyModbusTCP/client.py
+++ test/test_utilities/pyModbusTCP/client.py
@@ -4,26 +4,22 @@ from pyModbusTCP.client import ModbusClient as BaseClient
 
 
 class ModbusClient(BaseClient):
-    @staticmethod
-    def open() -> bool:
+    value = [0, 0]
+
+    def open(self) -> bool:
         return True
 
-    @staticmethod
-    def is_open() -> bool:
+    def is_open(self) -> bool:
         return True
 
-    @staticmethod
-    def close() -> None:
+    def close(self) -> None:
         pass
 
-    @staticmethod
-    def mode(val) -> None:
+    def mode(self, mode=None) -> None:
         pass
 
-    @staticmethod
-    def unit_id(val) -> None:
+    def unit_id(self, unit_id=None) -> None:
         pass
 
-    @staticmethod
-    def read_holding_registers(val, num) -> List[int]:
-        return [17171, 34363]
+    def read_holding_registers(self, reg_addr, reg_nb=1) -> List[int]:
+        return self.value
