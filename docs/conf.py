29069facebb8cc20ad44df0943cf2b300eee0614
Benedikt Grosch <b.grosch@ptw.tu-darmstadt.de>
2022-03-16 10:26:52 +0100

    Version 1.4b0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ project = "ETA Utility Functions"
 copyright = "ETA-Fabrik"
 
 # The full version, including alpha/beta/rc tags
-release = "v1.4.0"
+release = "v1.4b0"
 
 
 # -- General configuration ---------------------------------------------------
