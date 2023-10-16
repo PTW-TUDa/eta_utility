ee882c398e607754484ace3a068e01f0f5ba8e74
Julius Balzer <julius.balzer@stud.tu-darmstadt.de>
2023-10-16 11:29:51 +0000

    Connector tests update

--- test/test_connectors/test_sub_handlers.py
+++ test/test_connectors/test_sub_handlers.py
@@ -1,13 +1,13 @@
 import asyncio
 import pathlib
 from concurrent.futures import ThreadPoolExecutor
-from datetime import datetime
 
 import numpy as np
 import pandas as pd
 import pytest
 
 from eta_utility.connectors import CsvSubHandler, DFSubHandler, Node
+from eta_utility.connectors.base_classes import SubscriptionHandler
 
 sample_series = pd.Series(
     data=[1, 2, 3], index=pd.DatetimeIndex(["2020-11-05 10:00:00", "2020-11-05 10:00:01.1", "2020-11-05 10:00:01.7"])
@@ -32,21 +32,17 @@ def test_node2():
     return Node(name="SecondNode", url="", protocol="local")
 
 
-class TestCSVSubHandler:
-    async def push_values(self, handler: CsvSubHandler, test_node, test_node2):
-        try:
-            for num in range(6):
-                idx = num // 2 if num > 1 else 0
-                if num % 2 == 0:
-                    handler.push(test_node, sample_series.values[idx], sample_series.index[idx])
-                else:
-                    handler.push(test_node2, sample_series2.values[idx], sample_series2.index[idx])
-        finally:
-            try:
-                handler.close()
-            except AttributeError:
-                pass
+async def push_values(handler: SubscriptionHandler, test_node, test_node2):
+    for num in range(6):
+        idx = num // 2 if num > 1 else 0
+        if num % 2 == 0:
+            handler.push(test_node, sample_series.values[idx], sample_series.index[idx])
+        else:
+            handler.push(test_node2, sample_series2.values[idx], sample_series2.index[idx])
+    handler.close()
 
+
+class TestCSVSubHandler:
     def test_push_timeseries_to_csv(self, temp_dir, test_node, test_node2):
         file = temp_dir / "csv_test_output.csv"
         handler = CsvSubHandler(file, 0.5)
@@ -54,7 +50,7 @@ class TestCSVSubHandler:
         executor = ThreadPoolExecutor(max_workers=3)
         loop = asyncio.get_event_loop()
         loop.set_default_executor(executor)
-        loop.run_until_complete(self.push_values(handler, test_node, test_node2))
+        loop.run_until_complete(push_values(handler, test_node, test_node2))
         executor.shutdown()
 
         with pathlib.Path(file).open("r") as f:
@@ -100,25 +96,18 @@ class TestDFSubHandler:
 
         assert (data.values == sample_series.values[-1]).all()
 
-    async def push_loop_fillna(self, handler, test_node, test_node2):
-        for i in range(0, 3):
-            time1 = datetime.now()
-            handler.push(test_node, i, time1)
-            handler.push(test_node2, 2 * i + 10, time1)
-            await asyncio.sleep(1)
-            time2 = datetime.now()
-            handler.push(test_node2, 2 * i + 11, time2)
-            await asyncio.sleep(1)
-
     def test_auto_fillna(self, test_node, test_node2):
         # First test default behavior: nans are filled
-        handler = DFSubHandler(write_interval=1)
-        asyncio.get_event_loop().run_until_complete(self.push_loop_fillna(handler, test_node, test_node2))
+        handler = DFSubHandler(write_interval=0.25)  # Double the write interval to fill gaps with nans
+
+        # Push a value at time index[0] from node2, because there is no previous value for node2 to fill with
+        handler.push(test_node2, sample_series.values[0], sample_series.index[0])
+        asyncio.get_event_loop().run_until_complete(push_values(handler, test_node, test_node2))
 
         assert handler.data.notna().all().all()
 
         # Next test auto_fillna = False
-        handler = DFSubHandler(write_interval=1, auto_fillna=False)
-        asyncio.get_event_loop().run_until_complete(self.push_loop_fillna(handler, test_node, test_node2))
+        handler = DFSubHandler(write_interval=0.25, auto_fillna=False)
+        asyncio.get_event_loop().run_until_complete(push_values(handler, test_node, test_node2))
 
         assert handler.data.isna().any().any()
