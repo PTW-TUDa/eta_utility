d6c2e893f97303eb616d9aa90db9b374d436a1d7
Julius Balzer <julius.balzer@stud.tu-darmstadt.de>
2023-11-30 08:55:19 +0000

    Resolve "Subscription Tests for Modbus and OPC UA sometimes fail"

--- test/test_connectors/test_modbus.py
+++ test/test_connectors/test_modbus.py
@@ -319,32 +319,35 @@ class TestConnectorSubscriptions:
         with ModbusServer(ip=socket.gethostbyname(socket.gethostname()), port=config_modbus_port) as server:
             yield server
 
-    @pytest.fixture()
-    def _write_nodes_normal(self, server, local_nodes):
-        async def write_loop(server, local_nodes, values):
-            i = 0
-            while True:
-                server.write({node: values[node.name][i] for node in local_nodes})
-                # Index should fall back to one if the number of provided values is exceeded.
-                i = i + 1 if i < len(values[local_nodes[0].name]) - 1 else 0
-                await asyncio.sleep(1)
+    async def write_loop(self, server, local_nodes, values):
+        await asyncio.sleep(0.5)
+        for i in range(len(values["Serv.NodeName"])):
+            server.write({node: values[node.name][i] for node in local_nodes})
+            await asyncio.sleep(0.5)
 
-        asyncio.get_event_loop().create_task(write_loop(server, local_nodes, self.values))
-
-    def test_subscribe(self, local_nodes, _write_nodes_normal):
+    def test_subscribe(self, local_nodes, server):
         connection = ModbusConnection.from_node(local_nodes[0], usr="admin", pwd="0")
-        handler = DFSubHandler(write_interval=1)
-        connection.subscribe(handler, nodes=local_nodes, interval=1)
+        handler = DFSubHandler(write_interval=0.5)
+        connection.subscribe(handler, nodes=local_nodes, interval=0.5)
 
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(stop_execution(5))
+        loop.run_until_complete(self.write_loop(server, local_nodes, self.values))
 
+        # How many values are allowed to be missing in the dataframe
+        max_missing_values = 2
+
+        # Remove NaN values and round to 3 decimal places
+        data = handler.data.dropna().round(3)
+
+        # Check if all values are in the dataframe
         for node, values in self.values.items():
-            for idx, val in enumerate(values):
+            for value in values:
                 try:
-                    assert handler.data[node][idx] == pytest.approx(val, 0.01)
-                except IndexError:
-                    break
+                    assert value in data[node].array
+                except AssertionError as exception:
+                    max_missing_values -= 1
+                    if max_missing_values < 0:
+                        raise exception
 
         connection.close_sub()
 
